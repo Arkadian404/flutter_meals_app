40a724457236ddc42b3fd0c4eae84d02a17a55fd
ark <phutv1990@gmail.com>
2022-09-08 15:11:45 +0700

    create category stuffs

---
+++ lib/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Catergory(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Catergory(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Catergory(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Catergory(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Catergory(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Catergory(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Catergory(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Catergory(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Catergory(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Catergory(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
